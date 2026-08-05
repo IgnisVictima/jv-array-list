de3b8ced64b5b5f491fa7f3e14c8759609afd52e
Vitalii Smuraha <smuraga.v@gmail.com>
2026-08-05 23:13:28 +0200

    Final Work

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,120 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+
 public class ArrayList<T> implements List<T> {
-    @Override
-    public void add(T value) {
+    private Object[] elements;
+    private int size;
+
+    public ArrayList() {
+
+        elements = new Object[10];
+        size = 0;
 
     }
 
     @Override
+    public void add(T value) {
+        elements[size++] = value;
+    }
+
+    @Override
     public void add(T value, int index) {
+
+        if (index < 0 || index > size) {
+
+            throw new ArrayListIndexOutOfBoundsException("Out of the bounds exception!");
+
+        }
+
+        if (size == elements.length) {
+
+            Object[] elements_new = new Object[elements.length + elements.length / 2 + 1];
+
+            for (int i = 0; i < elements.length; i++) {
+
+                elements_new[i] = elements[i];
+
+            }
+
+            elements = elements_new;
+        }
+
+        for (int i = size; i > index; i--) {
+
+            elements[i] = elements[i - 1];
+        }
+
+        elements[index] = value;
+
+        size++;
 
     }
 
     @Override
     public void addAll(List<T> list) {
 
+        for (int i = 0; i < list.size(); i++) {
+
+            this.add(list.get(i));
+
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+
+        return (T) elements[index];
     }
 
     @Override
     public void set(T value, int index) {
 
+        elements[index] = value;
+
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Out of the bounds exception!");
+        }
+
+        T removed = (T) elements[index];
+
+        for (int i = index + 1; i < size; i++) {
+            elements[i - 1] = elements[i];
+        }
+
+        elements[--size] = null;
+        return removed;
     }
 
     @Override
     public T remove(T element) {
+
+        for (int i = 0; i < size; i++) {
+
+            if (elements[i] != null && elements[i].equals(element)) {
+
+                return remove(i);
+            }
+
+        }
+
         return null;
+
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+
+        return size == 0;
+
     }
 }
